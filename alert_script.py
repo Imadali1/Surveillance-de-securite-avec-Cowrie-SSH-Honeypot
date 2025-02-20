d3b8dcf588243e6e3ee6b5f6da8e27135c5c7f99
ImadHanw <168096882+Imadali1@users.noreply.github.com>
2025-02-20 22:43:20 +0100

    Update alert_script.py

--- alert_script.py
+++ alert_script.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 def send_slack_alert(message):
-    slack_webhook_url = 'https://hooks.slack.com/services/XXXX/XXXX/XXXX'  # Remplace par ton webhook
+    slack_webhook_url = ' https://hooks.slack.com/services/XXXX/XXXX/XXXX '
     payload = {'text': message}
-    requests.post(slack_webhook_url, json=payload)
+    headers = {'Content-Type': 'application/json'}
+    requests.post(slack_webhook_url, data=json.dumps(payload), headers=headers)
 
-# Exemple d'alerte
-send_slack_alert("Suspicious SSH login attempt detected on Cowrie!")
+send_slack_alert(" Suspicious SSH login attempt detected on Cowrie! ")
